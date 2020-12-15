bea7ee5e1eeb711ed38d5a0abda7e62b7291d3e5
Ali Farhat <alifar2@gmail.com>
2020-12-15 20:46:51 +0100

    Added cron to fetch new feeds each minute

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 var http = require('http');
+var cron = require('node-cron');
 var RSSCombiner = require('./utils/combine');
+
+var xml = null;
 
 var feedConfig = {
 	title: 'News Feed',
@@ -10,6 +13,7 @@ var feedConfig = {
 		'https://www.nu.nl/rss/Economie',
 		'https://www.nu.nl/rss/Tech',
 		'https://nl.investing.com/rss/news_285.rss',
+		'https://www.rtlnieuws.nl/tech?_format=rss',
 	],
 	pubDate: new Date(),
 	softFail: true,
@@ -17,15 +21,27 @@ var feedConfig = {
 
 RSSCombiner(feedConfig)
 	.then(function (combinedFeed) {
-		var xml = combinedFeed.xml();
-		http
-			.createServer(function (req, res) {
-				res.writeHead(200, { 'Content-Type': 'application/xhtml+xml' });
-				res.end(xml);
-			})
-			.listen(5000);
+		xml = combinedFeed.xml();
 	})
 	.catch(err => {
 		console.log(err);
 		res.status(200).send(err);
 	});
+
+cron.schedule('* * * * *', () => {
+	RSSCombiner(feedConfig)
+		.then(function (combinedFeed) {
+			xml = combinedFeed.xml();
+		})
+		.catch(err => {
+			console.log(err);
+			res.status(200).send(err);
+		});
+});
+
+http
+	.createServer(function (req, res) {
+		res.writeHead(200, { 'Content-Type': 'application/xhtml+xml' });
+		res.end(xml);
+	})
+	.listen(5000);
